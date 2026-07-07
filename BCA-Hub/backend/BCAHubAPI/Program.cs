fdb6b48e79e7b96dc06e8d5ecf6a7cf5d682edb4
additya <addityachoudhary62@gmail.com>
2026-07-07 12:16:27 +0530

    All files

--- BCA-Hub/backend/BCAHubAPI/Program.cs
+++ BCA-Hub/backend/BCAHubAPI/Program.cs
@@ -19,15 +19,16 @@ builder.Services.Configure<MongoDbSettings>(
 
 builder.Services.AddSingleton<MongoDbContext>();
 
-// OpenAPI
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
